b7ed01012cc27f802ddeb4af4ff9280adb0cc5ab
Aaron Steele <eightysteele@gmail.com>
2015-02-04 14:42:41 -0800

    @apercas yo yo i think this fixes the travis failure :D

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! sh
 
 # download and install Google App Engine Python SDK
-wget https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.15.zip
-sudo unzip -q -d /usr/local/ google_appengine_1.9.15.zip
+wget https://storage.googleapis.com/appengine-sdks/deprecated/1914/google_appengine_1.9.14.zip
+sudo unzip -q -d /usr/local/ google_appengine_1.9.14.zip
 
 pip install -r requirements.txt
