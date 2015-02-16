451810b21eec5c1b4dbac3abc30b48b62c3bbbe1
David González <david@simbiotica.es>
2015-02-16 12:15:21 +0100

    Updates sqls also in tests

--- test/gfw/forestchange/sqls.py
+++ test/gfw/forestchange/sqls.py
@@ -6,7 +6,7 @@ forma_subnational = "SELECT COUNT(f.*) AS value FROM forma_api f INNER JOIN ( SE
 
 forma_wdpa = "SELECT COUNT(f.*) AS value FROM forma_api f, (SELECT * FROM wdpa_all WHERE wdpaid=1) AS p WHERE ST_Intersects(f.the_geom, p.the_geom) AND f.date >= '2014-01-01'::date AND f.date <= '2015-01-01'::date"
 
-forma_use = "SELECT COUNT(f.*) AS value FROM logging_gcs_wgs84 u, forma_api f WHERE u.cartodb_id = 1 AND ST_Intersects(f.the_geom, u.the_geom) AND f.date >= '2014-01-01'::date AND f.date <= '2015-01-01'::date"
+forma_use = "SELECT COUNT(f.*) AS value FROM gfw_logging u, forma_api f WHERE u.cartodb_id = 1 AND ST_Intersects(f.the_geom, u.the_geom) AND f.date >= '2014-01-01'::date AND f.date <= '2015-01-01'::date"
 
 forma_begin = "SELECT COUNT(f.*) AS value FROM forma_api f WHERE f.date >= 'foo'::date AND f.date <= '2015-01-01'::date AND ST_INTERSECTS( ST_SetSRID( ST_GeomFromGeoJSON('foo'), 4326), f.the_geom)"
 
