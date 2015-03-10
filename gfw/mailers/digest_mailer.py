3e01dcae77136a9466267acdbfdcaf9ea455cd14
Brookie Guzder-Williams <brook.williams@gmail.com>
2015-03-10 11:02:12 +0100

    add mailers

---
+++ gfw/mailers/digest_mailer.py
@@ -0,0 +1,44 @@
+intro = """You have subscribed to forest change alerts through Global
+Forest Watch. This message reports new forest change alerts for one of your
+areas of interest (a country or self-drawn polygon).
+
+<br><br>
+h4 Digest:
+"""
+
+summary = """
+<br><br>
+<b>{name}:<b><br>
+
+A total of {value} {name} were detected within your area of interest in
+the past {interval}. Explore the details of this dataset on Global Forest
+Watch by <a href='{link}'>clicking here</a>.
+"""
+
+dump_summary = """
+<br><br>
+<b>{name}: does not have value<b><br>
+{params}
+"""
+
+
+
+outro = """
+<br><br>
+
+You can unsubscribe or manage your subscriptions by emailing: gfw@wri.org
+
+<br><br>
+
+You will receive a separate e-mail for each distinct polygon, country, or shape
+on the GFW map. You will also receive a separate e-mail for each dataset for
+which you have requested alerts (FORMA alerts, Imazon SAD Alerts, and NASA
+QUICC alerts.)
+
+<br><br>
+
+Please note that this information is subject to the Global Forest Watch <a
+href='http://globalforestwatch.com/terms'>Terms of Service</a>.
+"""
+
+
---
+++ gfw/mailers/subscribe_mailer.py
@@ -0,0 +1,5 @@
+subject = 'You subscribed to Global Forest Watch'
+body = """
+To receive monthly FORMA updates please confirm your subscription by replying to this email 
+or by clicking here:\n%s\n\nTerms of Service: http://globalforestwatch.org/terms
+"""
