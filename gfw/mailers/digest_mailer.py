ae0f9f0f7b02c9599c5c70689faf887ec87386b8
Brookie Guzder-Williams <brook.williams@gmail.com>
2015-04-24 15:50:21 -0700

    line breaks 2

--- gfw/mailers/digest_mailer.py
+++ gfw/mailers/digest_mailer.py
@@ -1,11 +1,10 @@
 intro = """
-<br><br>
-You have subscribed to receive alerts through <a href='http://www.globalforestwatch.org/'>Global Forest Watch</a>. This message reports new forest change alerts and user stories for the area of interest you selected (a country, subnational jurisdiction, or user-drawn shape). You will receive a separate email for each distinct area of interest you subscribe to.
 <br>
+You have subscribed to receive alerts through <a href='http://www.globalforestwatch.org/'>Global Forest Watch</a>. This message reports new forest change alerts and user stories for the area of interest you selected (a country, subnational jurisdiction, or user-drawn shape). You will receive a separate email for each distinct area of interest you subscribe to.
+<br><br>
 """
 
 header="""
-<br><br>
 <b>New alerts added to the GFW platform:</b>
 <br><br>
 """
@@ -29,6 +28,9 @@ There may be lag time between when forest change is detected and when alerts are
 <br>
 <br>
 Please note that this information is subject to the Global Forest Watch <a href='http://globalforestwatch.com/terms'>Terms of Service</a>. You can unsubscribe or manage your subscriptions by emailing gfw@wri.org. Please visit <a href='http://fires.globalforestwatch.org/#v=home&x=115&y=0&l=5&lyrs=Active_Fires'>GFW Fires</a> to subscribe to receive fire alerts. 
+<br>
+<br>
+<br>
 """
 
 link_geom = """http://www.globalforestwatch.org/map/3/{lat}/{lon}/ALL/grayscale/{url_id}?geojson={geom}&begin={min_date}&end={max_date}"""
