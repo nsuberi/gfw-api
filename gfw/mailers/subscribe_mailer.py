8db7388b35601f23099593780a463e7e3c9a5f0d
Brookie Guzder-Williams <brook.williams@gmail.com>
2015-05-07 11:39:11 -0700

    subscription email: remove explicit Forma

--- gfw/mailers/subscribe_mailer.py
+++ gfw/mailers/subscribe_mailer.py
@@ -1,5 +1,5 @@
 subject = 'You subscribed to Global Forest Watch'
 body = """
-To receive monthly FORMA updates please confirm your subscription by replying to this email 
-or by clicking here:\n%s\n\nTerms of Service: http://globalforestwatch.org/terms
+To receive monthly emails containing new forest change alerts and user stories for the area of interest you selected please confirm your subscription by replying to this 
+email or by clicking here:\n%s\n\nTerms of Service: http://globalforestwatch.org/terms
 """
