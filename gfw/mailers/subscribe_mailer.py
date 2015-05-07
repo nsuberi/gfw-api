54ea9cc0dd41a0d86662db078b0e31cb2c9e4c05
Brookie Guzder-Williams <brook.williams@gmail.com>
2015-05-07 12:58:01 -0700

    mailer update

--- gfw/mailers/subscribe_mailer.py
+++ gfw/mailers/subscribe_mailer.py
@@ -1,4 +1,4 @@
 subject = 'You subscribed to Global Forest Watch'
 body = """
-To receive monthly emails containing new forest change alerts and user stories for the area of interest you selected please confirm your subscription by replying to this email or by clicking here:\n\n%s\n\nTerms of Service: http://globalforestwatch.org/terms
+Click the link below (or reply to this email) to confirm your subscription to forest change alerts and user stories for the area of interest you selected:\n\n%s\n\nTerms of Service: http://globalforestwatch.org/terms
 """
