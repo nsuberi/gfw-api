bf862457d5328d14178e53670921cddbbf3a281b
Brookie Guzder-Williams <brook.williams@gmail.com>
2015-05-07 11:46:21 -0700

    subscription email: adding linebreaks

--- gfw/mailers/subscribe_mailer.py
+++ gfw/mailers/subscribe_mailer.py
@@ -1,4 +1,4 @@
 subject = 'You subscribed to Global Forest Watch'
 body = """
-To receive monthly emails containing new forest change alerts and user stories for the area of interest you selected please confirm your subscription by replying to this email or by clicking here:\n%s\n\nTerms of Service: http://globalforestwatch.org/terms
+To receive monthly emails containing new forest change alerts and user stories for the area of interest you selected please confirm your subscription by replying to this email or by clicking here:\n\n%s\n\nTerms of Service:\nhttp://globalforestwatch.org/terms
 """
