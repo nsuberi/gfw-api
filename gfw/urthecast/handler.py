eb06f27ab7907b0dc211afdc09aede2f5ead31c6
Alfred Cauthen <rashupee@gmail.com>
2015-09-22 09:10:05 -0700

    WIP, commiting to change branches

--- gfw/urthecast/handler.py
+++ gfw/urthecast/handler.py
@@ -6,68 +6,66 @@
 import webapp2
 import json
 
+from google.appengine.api import memcache
 from gfw.urthecast.api import Urthecast
-from gfw.urthecast.model import TilesUC
+# from gfw.urthecast.model import TilesUC
 
 uc = Urthecast()
 
 class UrthecastHandler(webapp2.RequestHandler):
-	
-	def _get_uri_params(uri_string):
+
+	def _get_uri_params(self,uri_string):
 		# returns uri parameters as a dictionary - only set to work on simple tiles...
-		# TODO extend to work with any uri for urthecast tiles
-		params = {}
+		# TODO extend to work with any uri for urthecast tiles - return uri_dict and params_dict
+		uri = {}
 		items = uri_string.split('/')
-		params['renderer']=items[0]
-		params['z']=items[1]
-		params['x']=items[2]
-		params['y']=items[3]
-		return params
+		uri['renderer']=items[1]
+		uri['z']=items[2]
+		uri['x']=items[3]
+		uri['y']=items[4]
+		return uri
 
-    def tiles(self,*args,**kwargs):
-        print "somethinig"
-        return
+	def tiles(self, *args, **kwargs):
+		self.response.headers['Content-Type'] = 'text/plain'
+		self.response.write('Hello, World!')
+		urthecast_url_part = self.request.path_qs.replace('urthecast/map-tiles/','')
+		print "**********"
+		print "**********"
+		print "**********"
+		print urthecast_url_part
+		p = self._get_uri_params(urthecast_url_part)
+		print p
+		tiles_key = "%s-tile-%s-%s-%s" % (p['renderer'],p['z'],p['x'],p['y'])
+		print tiles_key
+		print "**********"
+		print "**********"
+		print "**********"
+		# uct = TilesUC.search_for(cls,tiles_key)
+		uc.tiles(urthecast_url_part)
+		self._set_response('image/png')
 
-        # urthecast_url_part = self.request.path_qs.replace('urthecast/map-tiles/','')
-        # p = _get_uri_params(urthecast_url_part)
-        # key = "%s-tile-%s-%s-%s" % (p['renderer'],p['z'],p['x'],p['y'])
-        # print key
-        # uct = TilesUC.search_for(cls,key)
-        # uc.tiles(urthecast_url_part)
-        # self._set_response('image/png')}}
+	def _set_response(self,content_type):
+		if uc.error_message:
+			content_type = 'application/json'
+			response = json.dumps(uc.error_message)
+		else:
+			response = uc.data
+		self.response.headers.add('Content-Type', content_type)
+		self.response.write(response)
 
-    def archive(self,*args,**kwargs):
-        # https://api.urthecast.com/v1/archive/scenes
-        urthecast_url_part = self.request.path_qs.replace('urthecast/archive/','')
-        uc.scenes(urthecast_url_part)
-        self._set_response('application/json')
-        
-    def _set_response(self,content_type):
-        if uc.error_message:
-            content_type = 'application/json'
-            response = json.dumps(uc.error_message)
-        else:
-            response = uc.data
-        self.response.headers.add('Content-Type', content_type)
-        self.response.write(response)
+	def get_tiles(self, tiles_key):
+		# Expecting strings for a string tiles_key
+		tiles = memcache.get(tiles_key)
 
 routes = [
-        webapp2.Route(
-            r'/urthecast/map-tiles/<:.*>', 
-            handler=UrthecastHandler,
-            handler_method='tiles',
-            methods=['GET']
-        ),
+		webapp2.Route(
+			r'/urthecast/map-tiles/<:.*>', 
+			handler=UrthecastHandler,
+			handler_method='tiles',
+			methods=['GET']
+		)
+	]
 
-        webapp2.Route(
-            r'/urthecast/archive/<:.*>', 
-            handler=UrthecastHandler,
-            handler_method='archive',
-            methods=['GET']
-        )
-        
-    ]
-
-    
+	
 app = webapp2.WSGIApplication(routes, debug=True)
 
