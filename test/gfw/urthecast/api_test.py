8236f22d805b798471d65aa2a8deb48bf6988366
Alfred Cauthen <rashupee@gmail.com>
2015-10-02 14:45:14 -0700

    continuing with tests work

--- test/gfw/urthecast/api_test.py
+++ test/gfw/urthecast/api_test.py
@@ -1,4 +1,5 @@
 from test import common
+from appengine_config import runtime_config
 
 import unittest
 import webapp2
@@ -31,3 +32,32 @@ class UrthecastTest(unittest.TestCase):
 		self.valid_response = RequestsFaker(valid_dict)
 		self.params_dict = {'api_key': self.uc.key, 'api_secret': self.uc.secret}
 		self.fake_id = 'fake_id'
+		self.valid_tiles_response = 'Valid tiles response mockery'
+		self.fake_url_part = '/fake/as/hell'
+
+	def test___init__(self):
+		self.uc = urthecast.Urthecast('fake_key','fake_secret')
+		self.assertEqual(self.uc.key,'fake_key')
+		self.assertEqual(self.uc.secret,'fake_secret')
+		self.uc = urthecast.Urthecast()
+		self.assertIsNone(self.uc.data)
+		self.assertIsNone(self.uc.error_message)
+		self.assertEqual(self.uc.key,runtime_config.get("urthecast_key"))
+		self.assertEqual(self.uc.secret,runtime_config.get("urthecast_secret"))
+
+	@mock.patch('urllib2.Request')
+	def test_tiles(self, mock_Request_tiles):
+		mock_Request_tiles.return_value = self.valid_tiles_response
+		r = self.uc.tiles(self.fake_url_part)
+
+
+
+
+	@mock.patch('urllib2.Request')
+	def test_scenes(self, mock_Request_scenes):
+		self.assertEqual(2,3)
+
+
+
+if __name__ == '__main__':
+	unittest.main(exit=False, failfast=True)
