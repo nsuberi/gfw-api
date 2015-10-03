5a5d8128f0a5f3cbcc58c7a1e1cef9c38a575b56
Alfred Cauthen <rashupee@gmail.com>
2015-10-03 08:55:04 -0700

    WIP tests. Commented out unfinished tests

--- test/gfw/urthecast/api_test.py
+++ test/gfw/urthecast/api_test.py
@@ -10,6 +10,7 @@ import gfw.urthecast.api as urthecast
 import json
 import requests
 import mock
+import regex
 
 class RequestsFaker():
 	def __init__(self,json,status_code=None):
@@ -45,17 +46,19 @@ class UrthecastTest(unittest.TestCase):
 		self.assertEqual(self.uc.key,runtime_config.get("urthecast_key"))
 		self.assertEqual(self.uc.secret,runtime_config.get("urthecast_secret"))
 
-	@mock.patch('urllib2.Request')
-	def test_tiles(self, mock_Request_tiles):
-		mock_Request_tiles.return_value = self.valid_tiles_response
-		r = self.uc.tiles(self.fake_url_part)
+	# @mock.patch('urllib2.Request')
+	# def test_tiles(self, mock_Request_tiles):
+	# 	mock_Request_tiles.return_value = self.valid_tiles_response
+	# 	r = self.uc.tiles(self.fake_url_part)
+	# 	self.assert_called_with()
 
 
 
 
-	@mock.patch('urllib2.Request')
-	def test_scenes(self, mock_Request_scenes):
-		self.assertEqual(2,3)
+
+	# @mock.patch('urllib2.Request')
+	# def test_scenes(self, mock_Request_scenes):
+	# 	self.assertEqual(2,3)
 
 
 
