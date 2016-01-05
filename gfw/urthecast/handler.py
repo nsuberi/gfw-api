1a9bb0563bde74c4eda980084de173f0358b1c13
Adam Mulligan <adammulligan@gmail.com>
2016-01-05 11:14:52 +0000

    Adds Access-Control tiles for staging deployment access

--- gfw/urthecast/handler.py
+++ gfw/urthecast/handler.py
@@ -14,62 +14,68 @@ uc = Urthecast()
 
 class UrthecastHandler(webapp2.RequestHandler):
 
-	def get_data(self,urthecast_url_part):
-		expire = 3600
+        def get_data(self,urthecast_url_part):
+                expire = 3600
 
-		uc.tiles(urthecast_url_part)
+                uc.tiles(urthecast_url_part)
 
-		return uc.data
+                return uc.data
 
-		#### testing speed #####
+                #### testing speed #####
 
-		# data = memcache.get(urthecast_url_part)
-		# if data is not None:
-		# 	return data
-		# else:
-		# 	uc.tiles(urthecast_url_part)
-		# 	data = uc.data
-		# 	if data:
-		# 		memcache.add(urthecast_url_part, data, time=expire)
-		# return data
+                # data = memcache.get(urthecast_url_part)
+                # if data is not None:
+                #       return data
+                # else:
+                #       uc.tiles(urthecast_url_part)
+                #       data = uc.data
+                #       if data:
+                #               memcache.add(urthecast_url_part, data, time=expire)
+                # return data
 
-	def tiles(self, *args, **kwargs):
-		uc.data = None
-		uc.error_message = None
-		urthecast_url_part = self.request.path_qs.replace('urthecast/map-tiles/','')
-		data = self.get_data(urthecast_url_part)
-		self._set_response('image/png',data)
+        def tiles(self, *args, **kwargs):
+                uc.data = None
+                uc.error_message = None
+                urthecast_url_part = self.request.path_qs.replace('urthecast/map-tiles/','')
+                data = self.get_data(urthecast_url_part)
+                self._set_response('image/png',data)
 
-	def archive(self, *args, **kwargs):
-		# https://api.urthecast.com/v1/archive/scenes
-		urthecast_url_part = self.request.path_qs.replace('urthecast/archive/scenes/','')
-		response_dict = json.dumps(json.loads(uc.scenes(urthecast_url_part)))
-		self._set_response('application/json',response_dict)
+        def archive(self, *args, **kwargs):
+                # https://api.urthecast.com/v1/archive/scenes
+                urthecast_url_part = self.request.path_qs.replace('urthecast/archive/scenes/','')
+                response_dict = json.dumps(json.loads(uc.scenes(urthecast_url_part)))
+                self._set_response('application/json',response_dict)
 
-	def _set_response(self,content_type,data):
-		if not data:
-			content_type = 'application/json'
-			response = json.dumps(uc.error_message)
-		else:
-			response = data
-		self.response.headers.add('Content-Type', content_type)
-		self.response.write(response)
+        def _set_response(self,content_type,data):
+                if not data:
+                        content_type = 'application/json'
+                        response = json.dumps(uc.error_message)
+                else:
+                        response = data
+
+                self.response.headers['Content-Type'] = content_type
+                self.response.headers['Access-Control-Allow-Origin'] = '*'
+                self.response.headers['Access-Control-Allow-Headers'] = \
+                    'Origin, X-Requested-With, Content-Type, Accept'
+                self.response.headers['Access-Control-Allow-Methods'] = 'GET'
+
+                self.response.write(response)
 
 routes = [
-		webapp2.Route(
-			r'/urthecast/map-tiles/<:.*>', 
-			handler=UrthecastHandler,
-			handler_method='tiles',
-			methods=['GET']
-		),
-		webapp2.Route(
-			r'/urthecast/archive/scenes/<:.*>', 
-			handler=UrthecastHandler,
-			handler_method='archive',
-			methods=['GET']
-		)
-	]
+                webapp2.Route(
+                        r'/urthecast/map-tiles/<:.*>',
+                        handler=UrthecastHandler,
+                        handler_method='tiles',
+                        methods=['GET']
+                ),
+                webapp2.Route(
+                        r'/urthecast/archive/scenes/<:.*>',
+                        handler=UrthecastHandler,
+                        handler_method='archive',
+                        methods=['GET']
+                )
+        ]
 
-	
+
 app = webapp2.WSGIApplication(routes, debug=True)
 
