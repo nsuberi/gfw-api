b23ae44435df84ad7a011f184bc05922751e5444
Adam Mulligan <adammulligan@gmail.com>
2016-03-21 13:35:31 +0000

    Ignore fires subscriptions, and prevent migrations being created multiple times

--- gfw/v2/migrations/migration.py
+++ gfw/v2/migrations/migration.py
@@ -27,9 +27,14 @@ ALLOWED_TOPICS = [
     'alerts/landsat', 'alerts/glad'
 ]
 
+IGNORED_TOPICS = [
+    'updates/forma'
+]
+
 class Migration(ndb.Model):
     subscriptions = ndb.KeyProperty(repeated=True)
     email = ndb.StringProperty()
+    complete = ndb.BooleanProperty(default=False)
 
     kind = 'Migration'
 
@@ -42,12 +47,16 @@ class Migration(ndb.Model):
             migration = cls()
             migration.email = email
 
+        if migration.complete == True:
+            return
+
         subscriptions = Subscription.query(Subscription.email == email)
         for subscription in subscriptions.iter():
             if hasattr(subscription, 'user_id') and subscription.user_id != None: continue
             migration.subscriptions.append(subscription.key)
 
         if len(migration.subscriptions) > 0:
+            migration.complete = True
             migration.put()
 
     @classmethod
@@ -61,15 +70,20 @@ class Migration(ndb.Model):
     def update_subscriptions(self, user):
         for i, subscription_key in enumerate(self.subscriptions):
             subscription = subscription_key.get()
+
+            if subscription.topic in IGNORED_TOPICS:
+                continue
+
             subscription.user_id = user.key
             subscription.confirmed = True
+
+            if subscription.topic not in ALLOWED_TOPICS:
+                subscription.topic = 'alerts/treeloss'
+                subscription.params['topic'] = 'alerts/treeloss'
 
             subscription.params = subscription.params or {}
             subscription.params['tab'] = 'analysis-tab'
             subscription.url = runtime_config.get('GFW_BASE_URL') + \
                 gfw_map_url(subscription.params)
-
-            if subscription.topic not in ALLOWED_TOPICS:
-                subscription.topic = 'alerts/treeloss'
 
             subscription.put()
