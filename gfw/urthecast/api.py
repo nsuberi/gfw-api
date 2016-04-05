7297733c7e7563744999f3bb5d876330bd462046
Adam Mulligan <adammulligan@gmail.com>
2016-04-05 14:28:32 +0100

    Correctly indent urthecast and remove unused code

--- gfw/urthecast/api.py
+++ gfw/urthecast/api.py
@@ -9,68 +9,32 @@ from google.appengine.api import urlfetch
 
 class Urthecast:
 
-	def __init__(self, key=None, secret=None):
-		# Initializes an urthecastAPI caller based on a user's passed secrets
-		# or secrets stored in private dev.json and prod.json files
-		self.data = None
-		self.error_message = None
-		self.key = key or runtime_config.get("urthecast_key")
-		self.secret = secret or runtime_config.get("urthecast_secret")
+    def __init__(self, key=None, secret=None):
+        # Initializes an urthecastAPI caller based on a user's passed secrets
+        # or secrets stored in private dev.json and prod.json files
+        self.data = None
+        self.error_message = None
+        self.key = key or runtime_config.get("urthecast_key")
+        self.secret = secret or runtime_config.get("urthecast_secret")
 
-	def tiles(self,url_part):
-		s = random.choice(['a','b','c','d','e','f','g','h'])
-		url = 'https://tile-{s}.urthecast.com/v1/{url_part}'.format(s=s,url_part=url_part)
-		return self._request_url(url)
+    def tiles(self,url_part):
+        s = random.choice(['a','b','c','d','e','f','g','h'])
+        url = 'https://tile-{s}.urthecast.com/v1/{url_part}'.format(s=s,url_part=url_part)
+        return self._request_url(url)
 
-	def scenes(self,url_part):
-		url = 'https://api.urthecast.com/v1/archive/scenes{url_part}'.format(url_part=url_part)
-		return self._request_url(url)
+    def scenes(self,url_part):
+        url = 'https://api.urthecast.com/v1/archive/scenes{url_part}'.format(url_part=url_part)
+        return self._request_url(url)
 
-	def _request_url(self,url):
-		if '?' in url:
-			authorized_tmpl = "{url}&api_key={key}&api_secret={secret}"
-		else:
-			authorized_tmpl = "{url}?api_key={key}&api_secret={secret}"
+    def _request_url(self,url):
+        if '?' in url:
+            authorized_tmpl = "{url}&api_key={key}&api_secret={secret}"
+        else:
+            authorized_tmpl = "{url}?api_key={key}&api_secret={secret}"
 
-		authorized_url = authorized_tmpl.format(url=url,key=self.key,secret=self.secret)
+        authorized_url = authorized_tmpl.format(url=url,key=self.key,secret=self.secret)
 
-		###########testing improvements###########
-		urlfetch.set_default_fetch_deadline(30)
-		result = urlfetch.fetch(authorized_url)
-		self.data = result.content
-		return self.data
-		########### / testing improvements###########
-		max_retry = 2
-		retry_count = 0
-		retry = True
-		t = 5 # Time to wait between raised HTTPException and calling again
-
-		while retry:
-			retry = False
-			e = None
-			try:
-				result = urlfetch.fetch(authorized_url)
-				if result.status_code == 200:
-					self.data = result.content
-				else:
-					self.error_message = {'Code':result.status_code}
-					logging.error('********* THERE IS NO DATA but NO exception is thrown. status code is !=200 **********')
-			except urlfetch.InvalidURLError, e:
-				logging.error('********* Invalid url ***********: %s'%str(e))
-			except urlfetch.DownloadError:
-				retry_count += 1
-				if retry_count < max_retry:
-					logging.info('Trying again; re-attempt number '+str(retry_count))
-					retry = True
-					logging.info('Sleeping for '+str(t)+' seconds')
-		 			time.sleep(t)
-		 		else:
-					logging.info('Last retry also timed out.')
-		 			e = 'Multiple Timeouts'
-				logging.error('********** Download Error ************: %s'% str(e))
-			except Exception, e:
-				logging.error('********** Generic Exception ************: %s'%str(e))
-			if e:
-				self.error_message = {'Exception string':str(e)}
-		return self.data
-
+        urlfetch.set_default_fetch_deadline(30)
+        result = urlfetch.fetch(authorized_url)
+        self.data = result.content
+        return self.data
--- gfw/urthecast/handler.py
+++ gfw/urthecast/handler.py
@@ -8,74 +8,62 @@ import json
 
 from google.appengine.api import memcache
 
+from gfw import common
 from gfw.urthecast.api import Urthecast
 
 uc = Urthecast()
 
 class UrthecastHandler(webapp2.RequestHandler):
 
-        def get_data(self,urthecast_url_part):
-                expire = 3600
+    def get_data(self,urthecast_url_part):
+        expire = 3600
 
-                uc.tiles(urthecast_url_part)
+        uc.tiles(urthecast_url_part)
 
-                return uc.data
+        return uc.data
 
-                #### testing speed #####
+    def tiles(self, *args, **kwargs):
+        uc.data = None
+        uc.error_message = None
+        urthecast_url_part = self.request.path_qs.replace('urthecast/map-tiles/','')
+        data = self.get_data(urthecast_url_part)
+        self._set_response('image/png',data)
 
-                # data = memcache.get(urthecast_url_part)
-                # if data is not None:
-                #       return data
-                # else:
-                #       uc.tiles(urthecast_url_part)
-                #       data = uc.data
-                #       if data:
-                #               memcache.add(urthecast_url_part, data, time=expire)
-                # return data
+    def archive(self, *args, **kwargs):
+        # https://api.urthecast.com/v1/archive/scenes
+        urthecast_url_part = self.request.path_qs.replace('urthecast/archive/scenes/','')
+        response_dict = json.dumps(json.loads(uc.scenes(urthecast_url_part)))
+        self._set_response('application/json',response_dict)
 
-        def tiles(self, *args, **kwargs):
-                uc.data = None
-                uc.error_message = None
-                urthecast_url_part = self.request.path_qs.replace('urthecast/map-tiles/','')
-                data = self.get_data(urthecast_url_part)
-                self._set_response('image/png',data)
+    def _set_response(self,content_type,data):
+        if not data:
+            content_type = 'application/json'
+            response = json.dumps(uc.error_message)
+        else:
+            response = data
 
-        def archive(self, *args, **kwargs):
-                # https://api.urthecast.com/v1/archive/scenes
-                urthecast_url_part = self.request.path_qs.replace('urthecast/archive/scenes/','')
-                response_dict = json.dumps(json.loads(uc.scenes(urthecast_url_part)))
-                self._set_response('application/json',response_dict)
+        self.response.headers['Content-Type'] = content_type
+        self.response.headers['Access-Control-Allow-Origin'] = '*'
+        self.response.headers['Access-Control-Allow-Headers'] = \
+            'Origin, X-Requested-With, Content-Type, Accept'
+        self.response.headers['Access-Control-Allow-Methods'] = 'GET'
 
-        def _set_response(self,content_type,data):
-                if not data:
-                        content_type = 'application/json'
-                        response = json.dumps(uc.error_message)
-                else:
-                        response = data
-
-                self.response.headers['Content-Type'] = content_type
-                self.response.headers['Access-Control-Allow-Origin'] = '*'
-                self.response.headers['Access-Control-Allow-Headers'] = \
-                    'Origin, X-Requested-With, Content-Type, Accept'
-                self.response.headers['Access-Control-Allow-Methods'] = 'GET'
-
-                self.response.write(response)
+        self.response.write(response)
 
 routes = [
-                webapp2.Route(
-                        r'/urthecast/map-tiles/<:.*>',
-                        handler=UrthecastHandler,
-                        handler_method='tiles',
-                        methods=['GET']
-                ),
-                webapp2.Route(
-                        r'/urthecast/archive/scenes/<:.*>',
-                        handler=UrthecastHandler,
-                        handler_method='archive',
-                        methods=['GET']
-                )
-        ]
+    webapp2.Route(
+        r'/urthecast/map-tiles/<:.*>',
+        handler=UrthecastHandler,
+        handler_method='tiles',
+        methods=['GET']
+    ),
+    webapp2.Route(
+        r'/urthecast/archive/scenes/<:.*>',
+        handler=UrthecastHandler,
+        handler_method='archive',
+        methods=['GET']
+    )
+]
 
 
-app = webapp2.WSGIApplication(routes, debug=True)
-
+app = webapp2.WSGIApplication(routes, debug=common.IS_DEV)
